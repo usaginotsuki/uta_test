e1f8a8f4d899bccdffad36cb9af66447b3b109b2
usaginotsuki <jcchachalo@gmail.com>
2022-06-16 10:11:50 -0500

    Conexión a la base de datos

--- views/interfaces/servicios.php
+++ views/interfaces/servicios.php
@@ -4,114 +4,48 @@
     <title> UTA </title>
     <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="css/estilos.css">
-    <link rel="stylesheet" type="text/css" href="jquery-easyui-1.10.1/themes/default/easyui.css">
-    <link rel="stylesheet" type="text/css" href="jquery-easyui-1.10.1/themes/icon.css">
-    <link rel="stylesheet" type="text/css" href="jquery-easyui-1.10.1/themes/color.css">
-
-    <script type="text/javascript" src="jquery-easyui-1.10.1/jquery.min.js"></script>
-    <script type="text/javascript" src="jquery-easyui-1.10.1/jquery.easyui.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 
 <body>
-    <h2>UTA ESTUDIANTES</h2>
-    <button id="btn_add" onclick="location.href = 'views/interfaces/report.php'">Reporte</button>
-    <table id="dg" title="My Users" class="easyui-datagrid" style="width:700px;height:250px" url="models/cargar.php" toolbar="#toolbar" pagination="true" rownumbers="true" fitColumns="true" singleSelect="true" data-options="emptyMsg:'No se encontró nada'">
-        <thead>
-            <tr>
-                <th field="est_cedula" width="30">CEDULA</th>
-                <th field="est_nombre" width="30">NOMBRE</th>
-                <th field="est_apellido" width="30">APELLIDO</th>
-                <th field="est_direccion" width="30">DIRECCION</th>
-                <th field="est_telefono" width="30">TELEFONO</th>
+    <h2>Reporte de bares</h2>
 
-            </tr>
-        </thead>
-    </table>
-    <div id="toolbar">
-        <a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-add" plain="true" onclick="newUser()">Nuevo Estudiantes</a>
-        <a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-edit" plain="true" onclick="editUser()">Editar Estudiante</a>
-        <a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-remove" plain="true" onclick="destroyUser()">Borrar Estudiante</a>
-    </div>
+    <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "utaestudiantes";
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    $mysqli = new mysqli($servername, $username, $password, $dbname);
+    if (!$mysqli) {
+        die("Error en la conexión" . mysqli_connect_error());
+    }
 
-    <div id="dlg" class="easyui-dialog" style="width:400px" data-options="closed:true,modal:true,border:'thin',buttons:'#dlg-buttons'">
-        <form id="fm" method="post" novalidate style="margin:0;padding:20px 50px">
-            <h3>Información del estudiante</h3>
-            <div style="margin-bottom:10px">
-                <input name="est_cedula" class="easyui-textbox" required="true" label="Cédula:" style="width:100%">
-            </div>
-            <div style="margin-bottom:10px">
-                <input name="est_nombre" class="easyui-textbox" required="true" label="Nombre:" style="width:100%">
-            </div>
-            <div style="margin-bottom:10px">
-                <input name="est_apellido" class="easyui-textbox" required="true" label="Apellido:" style="width:100%">
-            </div>
-            <div style="margin-bottom:10px">
-                <input name="est_direccion" class="easyui-textbox" required="true" label="Dirección:" style="width:100%">
-            </div>
-            <div style="margin-bottom:10px">
-                <input name="est_telefono" class="easyui-textbox" label="Teléfono:" style="width:100%">
-            </div>
-        </form>
-    </div>
-    <div id="dlg-buttons">
-        <a href="javascript:void(0)" class="easyui-linkbutton c6" iconCls="icon-ok" onclick="saveUser()" style="width:90px">Guardar</a>
-        <a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-cancel" onclick="javascript:$('#dlg').dialog('close')" style="width:90px">Cancelar</a>
-    </div>
-    <script type="text/javascript">
-        var url;
+    $sql = "select * from bar";
+    $respuesta = $conn->query($sql);
+    $result = array();
 
-        function newUser() {
-            $('#dlg').dialog('open').dialog('center').dialog('setTitle', 'Nuevo Estudiante');
-            $('#fm').form('clear');
-            url = 'models/guardar.php';
-        }
+    while ($estudiantes = $respuesta->fetch_assoc()) {
+        array_push($result, $estudiantes);
+    }
 
-        function editUser() {
-            var row = $('#dg').datagrid('getSelected');
-            if (row) {
-                $('#dlg').dialog('open').dialog('center').dialog('setTitle', 'Editar Estudiante');
-                $('#fm').form('load', row);
-                url = 'models/editar.php';
-
-            }
-        }
-
-        function saveUser() {
-            $('#fm').form('submit', {
-                url: url,
-                iframe: false,
-                onSubmit: function() {
-                    return $(this).form('validate');
-                },
-                success: function(result) {
-                    var result = eval('(' + result + ')');
-                    if (result.errorMsg) {
-                        $.messager.show({
-                            title: 'Error',
-                            msg: result.errorMsg
-                        });
-                    } else {
-                        $('#dlg').dialog('close'); // close the dialog
-                        $('#dg').datagrid('reload'); // reload the user data
-                    }
-                }
-            });
-        }
-
-        function destroyUser() {
-            $('#fm').form('load', row);
-            url = 'models/editar.php';
-            var row = $('#dg').datagrid('getSelected');
-            if (row) {
-                $('#dg').datagrid('deleteRow', $('#dg').datagrid('getRowIndex', row));
-                $.post('models/eliminar.php', {
-                    est_cedula: row.est_cedula
-                }, function(data) {
-                    $('#dg').datagrid('reload');
-                });
-            }
-        }
-    </script>
+    ?>
+    <?php if (count($result) > 0) : ?>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th><?php echo implode('</th><th>', array_keys(current($result))); ?></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($result as $row) : array_map('htmlentities', $row); ?>
+                    <tr>
+                        <td><?php echo implode('</td><td>', $row); ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    <?php endif; ?>
 
 </body>
 
--- views/template.php
+++ views/template.php
@@ -14,9 +14,7 @@
     <nav>
         <ul>
             <li><a href="index.php?action=inicio">Inicio</a></li>
-            <li><a href="index.php?action=nosotros">Nosotros2</a></li>
             <li><a href="index.php?action=servicios">Servicios</a></li>
-            <li><a href="index.php?action=contactanos">Contactanos</a></li>
         </ul>
     </nav>
     <section>
